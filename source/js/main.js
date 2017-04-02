7fe9374614face298622f5d97d1a52b2a9836ee0
Journey1969 <journey1969@163.com>
2017-04-02 12:02:53 +0800

    add images placeholder

--- source/js/main.js
+++ source/js/main.js
@@ -1,24 +1,33 @@
 (function($) {
     // add code type
-    $('.highlight').each(function () {
+    $('.highlight').each(function() {
         var code_type = $(this).attr("class").split(' ')[1].toUpperCase();
         $(this).attr("data-code-type", code_type);
     })
 
     // add img wrapper and caption
-    $('.post-content').each(function () {
-        $(this).find('img').each(function () {
-            $(this).wrap('<div class="img-wrapper"></div>');
-            $(this).after('<span class="img-caption">' + this.title + '</span>');
+    $('.post-content').each(function() {
+        $(this).find('img').each(function() {
+            var img = $(this);
+            img.attr('class', 'lazy');
+            img.attr('data-original', img.attr('src'));
+            img.wrap('<div class="img-wrapper img-loading"></div>');
         });
     })
 
-    // cancel min-height of img wrapper after images all loaded
-    $('#main-container').imagesLoaded().done(function () {
-        $('.post-content').each(function () {
-            $(this).find('.img-wrapper').each(function () {
-                $(this).css('min-height', '0')
-            })
-        })
+    // cancel height of img wrapper after each image is loaded
+    $('#main-container').imagesLoaded().progress(function(instance, image) {
+        var wrapper = $(image.img).parent();
+        if (image.isLoaded) {
+            $(image.img).after('<p class="img-caption">' + image.img.alt + '</p>');
+        } else {
+            wrapper.addClass('img-broken');
+        }
+        wrapper.removeClass('img-loading');
     })
+
+    // lazyload
+    $(function() {
+        $("img.lazy").lazyload();
+    });
 })(jQuery);
