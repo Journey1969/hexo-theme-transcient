353c5b864f2cbe1a24328a1bed726ba28612e94c
Journey1969 <journey1969@163.com>
2017-04-03 17:27:08 +0800

    improve lazyload performance

--- README.md
+++ README.md
@@ -24,6 +24,13 @@ Here is the [Preview](http://journey1969.com)
 
 - [fancybox.js](http://fancybox.net/)
 
+## To Improve
+
+- 在`config.yml`中开启了[资源文件管理功能](https://hexo.io/zh-cn/docs/asset-folders.html)后，使用以下方式引用的图片将会进行**懒加载**和**原图预加载**，而用`![](image.jpg)`的方式引用则只会正常加载。
+  ```
+  {% asset_img example.jpg This is an example image %}
+  ```
+
 ## TODO
 
 [√] basic theme style
--- source/js/main.js
+++ source/js/main.js
@@ -1,48 +1,90 @@
 (function($) {
-    // add code type
-    $('.highlight').each(function() {
-        var code_type = $(this).attr("class").split(' ')[1].toUpperCase();
-        $(this).attr("data-code-type", code_type);
-    })
+    // window onload event flow
+    /*function addLoadEvent(func) {
+        var old_onload = window.onload;
+        if (typeof window.onload != 'function') {
+            window.onload = func;
+        } else {
+            window.onload = function () {
+                old_onload();
+                func();
+            }
+        }
+    }*/
 
     // add img wrapper and caption
-    $('.post-content').each(function(i) {
-        $(this).find('img').each(function() {
-            var img = $(this);
-            var origin_img_src = img.attr('src');
+    function wrapImages() {
+        $('.post-content').each(function(i) {
+            $(this).find('img').each(function() {
+                var img = $(this);
+                var origin_img_src = img.attr('src');
+                var external_img = img.parent()[0].nodeName === "P";
 
-            // create compressed images' source path
-            var path = origin_img_src.split('/');
-            path.push("min__" + path.pop());
-            min_img_src = path.join("/");
-            console.log(min_img_src);
+                // create compressed image's source path
+                var path = origin_img_src.split('/');
+                path.push("min__" + path.pop());
+                min_img_src = path.join("/");
 
-            // img.attr('class', 'lazy');
-            // img.attr('data-original', min_img_src);
-            img.attr('src', min_img_src);
-            img.wrap('<div class="img-wrapper img-loading"></div>');
-            img.after('<p class="img-caption">' + this.alt + '</p>');
-            img.wrap('<a href="' + origin_img_src + '" title="' + this.alt + '" class="fancybox" rel="article"></a>');
-        });
-    })
+                // start decorating img
+                img.addClass('lazy');
+                img.attr('data-original', external_img ? origin_img_src : min_img_src);
+                img.attr('src', external_img ? origin_img_src : '/images/loading.png');
 
-    // cancel height of img wrapper after each image is loaded
-    $('#main-container').imagesLoaded().progress(function(instance, image) {
-        var wrapper = $(image.img).parents('.img-wrapper');
-        if (!image.isLoaded) {
-            wrapper.addClass('img-broken');
-            wrapper.empty();
-        }
-        wrapper.removeClass('img-loading');
-    })
+                // start wrapper
+                img.wrap('<div class="img-wrapper"></div>');
+                img.after('<p class="img-caption">' + this.alt + '</p>');
+                img.wrap('<a href="' + origin_img_src + '" title="' + this.alt + '" class="fancybox" rel="article"></a>');
+
+                // start loading original image after this image is lazyloaded
+                img.on('load', function(err) {
+                    if (img.attr('src') !== '/images/loading.png'){
+                        $(this).addClass('img-loaded');
+                        $(this).css('background-image', 'url(' + img.attr('src').replace('min__', '') + ')');
+                    }
+                })
+            });
+        })
+    }
+
+    // replace .fancybox by .image-not-found when image is broken
+    function handleImagesNotFound() {
+        $('.post-content').each(function () {
+            $(this).imagesLoaded().progress(function(instance, image) {
+                var wrapper = $(image.img).parents('.img-wrapper');
+                if (!image.isLoaded) {
+                    wrapper.addClass('img-broken');
+                    wrapper.find('.fancybox').remove();
+                    wrapper.prepend('<img class="img-not-found" src="/images/image-not-found.png">')
+                }
+            })
+        })
+    }
+
+    // add code type
+    function addCodeType() {
+        $('.highlight').each(function() {
+            var code_type = $(this).attr("class").split(' ')[1].toUpperCase();
+            $(this).attr("data-code-type", code_type);
+        })
+    }
 
     // activate lazyload
-    /*
-    $(function() {
-        $("img.lazy").lazyload();
-    });
-    */
+    function activateLazyload() {
+        $('.lazy').lazyload({
+            effect: 'fadeIn',
+            threshold : 200
+        });
+    }
 
     // activate fancybox
-    $('a.fancybox').fancybox();
+    function activateFancybox() {
+        $('a.fancybox').fancybox();
+    }
+
+    wrapImages();
+    activateLazyload();
+    handleImagesNotFound();
+    activateFancybox();
+    addCodeType();
+
 })(jQuery);
